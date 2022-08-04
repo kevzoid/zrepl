4335d5e7bd4e4a933590cefc3f7dbb5e9c8ea4b4
Kevin Rabaev <kevzoid@gmail.com>
2022-08-04 14:22:50 +0300

    preload more modules

--- znode.js
+++ znode.js
@@ -7,7 +7,8 @@ const zon_root = path.join(os.homedir(), 'zon1');
 const zon_file = file=>path.join(zon_root, file);
 require(zon_file('./pkg/util/config.js'));
 
-const utils = 'etask string date csv exec json stream util'.split(/\s+/);
+const utils = `etask string date csv exec json stream util cli zdot zerr url
+    rand queue os`.split(/\s+/);
 console.log(`Loaded modules: ${['lodash'].concat(utils)}`);
 const r = repl.start(`Welcome to Node ${process.version}\n$ `);
 for (let util of utils)
